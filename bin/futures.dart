a3c3d01345341495c4b85e0ee031e66c375bf065
mjr2595 <michaeljohnraymond@gmail.com>
2023-10-10 23:46:20 -0500

    rest of course

---
+++ bin/futures.dart
@@ -0,0 +1,18 @@
+import 'dart:async';
+
+void main() {
+  var delay = Future.delayed(Duration(seconds: 5));
+
+  delay
+      .then((value) => print('I have been waiting'))
+      .catchError((err) => print(err));
+
+  print('I am not waiting');
+  print(runInTheFuture().toString());
+}
+
+Future<String> runInTheFuture() async {
+  var data = await Future.value('world');
+
+  return 'hello $data';
+}
